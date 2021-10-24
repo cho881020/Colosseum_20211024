3ac094e27e464312de42d74603659a95954ce1e2
조경진 <cho881020@gmail.com>
2021-10-24 12:03:26 +0900

    코틀린에서, xml연결하는 binding 변수 세팅

--- app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
 }
