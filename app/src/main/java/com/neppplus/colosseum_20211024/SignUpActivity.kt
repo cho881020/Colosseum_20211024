916703b1e47a7b6db4c8450f82f8929f1a02eaa2
조경진 <cho881020@gmail.com>
2021-10-31 11:01:53 +0900

    회원가입 화면도 BaseActivity 상속

--- app/src/main/java/com/neppplus/colosseum_20211024/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/SignUpActivity.kt
@@ -5,12 +5,23 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211024.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     lateinit var binding : ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
