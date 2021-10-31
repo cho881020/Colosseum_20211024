df6a7142dd1784fabcaba20c0fa49f81dd54ff6e
조경진 <cho881020@gmail.com>
2021-10-31 12:05:20 +0900

    회원가입 실패시 - 사유 토스트로

--- app/src/main/java/com/neppplus/colosseum_20211024/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/SignUpActivity.kt
@@ -2,8 +2,11 @@ package com.neppplus.colosseum_20211024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211024.databinding.ActivitySignUpBinding
+import com.neppplus.colosseum_20211024.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -25,7 +28,32 @@ class SignUpActivity : BaseActivity() {
             val inputNickname = binding.nicknameEdt.text.toString()
 
 //            입력 데이터를 => 서버의 회원가입 기능에 요청. => ServerUtil 함수 활용. => 함수가 아직 없으니 추가로 만들자.
-//            ServerUtil.
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    jsonObj 분석 -> UI 반영 코드만 작성.
+
+//                    code : 성공 (200) / 실패 (그 외) 여부.
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+//                        회원가입 성공.
+                    }
+                    else {
+//                        회원가입 실패. => 왜? message String에 담겨있을 예정.
+                        val message = jsonObj.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+
+
+                }
+
+            })
 
         }
 
