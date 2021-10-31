d3229afc46673a88c2e57f9b9175fabeb6819489
조경진 <cho881020@gmail.com>
2021-10-31 12:23:35 +0900

    로그인 성공시 - 닉네임 받아와서 출력

--- app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
@@ -55,8 +55,13 @@ class MainActivity : BaseActivity() {
 
                     if (code == 200) {
 
+//                        응용문제. 로그인 한 사람의 닉네임 추출 -> "~~님, 환영합니다!" 토스트 출력
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val userObj = dataObj.getJSONObject("user")
+                        val nickname = userObj.getString("nick_name")
+
                         runOnUiThread {
-                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "${nickname}님, 환영합니다!", Toast.LENGTH_SHORT).show()
                         }
 
                     }
