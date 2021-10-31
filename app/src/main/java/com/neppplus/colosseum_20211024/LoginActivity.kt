3beef79c7449f4969b87d03e3a1004499cd87efd
조경진 <cho881020@gmail.com>
2021-10-31 15:04:19 +0900

    로그인화면에서 -> 실제 토큰 저장

--- app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.colosseum_20211024.databinding.ActivityLoginBinding
+import com.neppplus.colosseum_20211024.utils.ContextUtil
 import com.neppplus.colosseum_20211024.utils.ServerUtil
 import org.json.JSONObject
 
@@ -62,6 +63,7 @@ class LoginActivity : BaseActivity() {
 
 //                        SharedPreferences 활용하여 저장해두자. => 필요할때 꺼내쓰도록.
 
+                        ContextUtil.setToken(mContext, token)
 
                         runOnUiThread {
                             Toast.makeText(mContext, "${nickname}님, 환영합니다!", Toast.LENGTH_SHORT).show()
