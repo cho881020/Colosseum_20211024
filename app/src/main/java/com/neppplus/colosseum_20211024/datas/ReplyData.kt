d0b56bd1365ea6023d84f6d08dcd2701b9905ea7
nepp-plus <nepp.plus@gmail.com>
2021-11-07 16:19:53 +0900

    댓글 상세 정보 파싱 준비  - 표현할 UI만 우선 작업

--- app/src/main/java/com/neppplus/colosseum_20211024/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/datas/ReplyData.kt
@@ -1,6 +1,8 @@
 package com.neppplus.colosseum_20211024.datas
 
 class ReplyData(var id: Int, var content: String) {
+
+//    var user : UserData  댓글을 적은 사람 정보 => 하위 데이터로.
     
     constructor() : this(0, "내용없음")
     
