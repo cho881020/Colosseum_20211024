822b007ce767268d22e02d5fc21e068b4bb48851
nepp-plus <nepp.plus@gmail.com>
2021-11-07 16:08:40 +0900

    새로고침 구현  - 서버에서 토론 상세 불러오기 재활용  - UI 문구 교체 함수 별도 작성

--- app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
@@ -36,9 +36,8 @@ class ViewTopicDetailActivity : BaseActivity() {
 
             ServerUtil.postRequestVote(mContext, mTopicData.sideList[0].id, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(jsonObj: JSONObject) {
-
-
-
+//                    새로 토론 상태 불러오기
+                    getTopicDetailFromServer()
                 }
 
             })
@@ -48,7 +47,8 @@ class ViewTopicDetailActivity : BaseActivity() {
         binding.voteToSecondSideBtn.setOnClickListener {
             ServerUtil.postRequestVote(mContext, mTopicData.sideList[1].id, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(jsonObj: JSONObject) {
-
+//                    새로 토론 상태 불러오기
+                    getTopicDetailFromServer()
                 }
 
             })
@@ -93,6 +93,12 @@ class ViewTopicDetailActivity : BaseActivity() {
 
                 mTopicData = TopicData.getTopicDataFromJSON(topicObj)
 
+//                새 mTopicData에 들어있는 데이터를 UI에 다시 반영.
+                runOnUiThread {
+                    refreshUI()
+                }
+
+
                 val repliesArr = topicObj.getJSONArray("replies")
 
                 for ( i   in   0 until  repliesArr.length()  ) {
@@ -120,6 +126,15 @@ class ViewTopicDetailActivity : BaseActivity() {
             }
 
         })
+
+    }
+
+
+    fun refreshUI() {
+
+//        득표 수 등은 자주 변경되는 데이터.
+        binding.firstSideVoteCountTxt.text = "${mTopicData.sideList[0].voteCount}표"
+        binding.secondSideVoteCountTxt.text = "${mTopicData.sideList[1].voteCount}표"
 
     }
 
