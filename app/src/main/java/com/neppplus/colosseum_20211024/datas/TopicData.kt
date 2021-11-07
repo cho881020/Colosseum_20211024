42dff49cd9cd1ab96cac16620d49f7e4a37544f8
nepp-plus <nepp.plus@gmail.com>
2021-11-07 14:59:29 +0900

    TopicData 클래스 기능 추가  - JSON 넣으면 TopicData로 변환해주는 기능

--- app/src/main/java/com/neppplus/colosseum_20211024/datas/TopicData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/datas/TopicData.kt
@@ -1,5 +1,6 @@
 package com.neppplus.colosseum_20211024.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class TopicData (
@@ -11,5 +12,29 @@ class TopicData (
 //    다른 형태의 생성자 (보조 생성자) 도 추가 지원.
 
     constructor() : this(0, "제목없음", "주소없음")
+
+    companion object {
+
+//        매번 파싱 코드를 화면마다 짜기가 번거롭다.
+//        TopicData의 기능으로 -> 토론에대한 내용을 갖고있는 jsonObj를 넣으면
+//          => 파싱을 진행해서 -> TopicData 형태로 변환해주는 기능.
+
+//        다른 화면에서는 jsonObj만 파싱해서 => 변환 기능으로 활용만.
+
+        fun getTopicDataFromJSON( jsonObj : JSONObject ) : TopicData {
+
+            val topicData = TopicData()
+
+            topicData.id = jsonObj.getInt("id")
+            topicData.title = jsonObj.getString("title")
+            topicData.imageURL = jsonObj.getString("img_url")
+
+            return topicData
+
+        }
+
+
+    }
+
 
 }
