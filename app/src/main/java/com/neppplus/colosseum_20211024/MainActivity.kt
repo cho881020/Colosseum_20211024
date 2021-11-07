c9746217c20aef93494a3abecaf7687c893597b8
nepp-plus <nepp.plus@gmail.com>
2021-11-07 11:28:55 +0900

    어댑터 객체 생성 / 화면에 기본 UI 표시

--- app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211024.adapters.TopicAdapter
 import com.neppplus.colosseum_20211024.databinding.ActivityMainBinding
 import com.neppplus.colosseum_20211024.datas.TopicData
 import com.neppplus.colosseum_20211024.utils.ServerUtil
@@ -12,6 +13,8 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
+
+    lateinit var mTopicAdapter : TopicAdapter
 
     val mTopicList = ArrayList<TopicData>()
 
@@ -31,6 +34,10 @@ class MainActivity : BaseActivity() {
 //        /v2/main_info API가 토론 주제 목록을 내려줌.
 //        서버 호출 => 파싱해서, mTopicList를 채워주자.
         getTopicListFromServer()
+
+
+        mTopicAdapter = TopicAdapter(mContext, R.layout.topic_list_item, mTopicList)
+        binding.topicListView.adapter = mTopicAdapter
 
 
     }
