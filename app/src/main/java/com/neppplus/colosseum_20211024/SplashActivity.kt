2fd80beb3ca2238275ec3c1d7a7d838ad325a997
nepp-plus <nepp.plus@gmail.com>
2021-11-07 12:05:38 +0900

    SplashActivity 추가  - 데이터 확인 화면

---
+++ app/src/main/java/com/neppplus/colosseum_20211024/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.neppplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211024.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
