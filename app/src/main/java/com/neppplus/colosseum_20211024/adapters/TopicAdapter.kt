69f7763651139624c608b620a804bb80f8ea190d
nepp-plus <nepp.plus@gmail.com>
2021-11-07 10:42:54 +0900

    어댑터 기초 작업 마무리

--- app/src/main/java/com/neppplus/colosseum_20211024/adapters/TopicAdapter.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/adapters/TopicAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.colosseum_20211024.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.colosseum_20211024.R
 import com.neppplus.colosseum_20211024.datas.TopicData
 
 class TopicAdapter(
@@ -9,5 +13,18 @@ class TopicAdapter(
     resId: Int,
     val mList: List<TopicData>) : ArrayAdapter<TopicData>(mContext, resId, mList) {
 
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.topic_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 
 }
