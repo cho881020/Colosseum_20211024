ef43fc3bb14d0b0c0171d09bfe79bba582ed8447
nepp-plus <nepp.plus@gmail.com>
2021-11-07 15:20:30 +0900

    선택 진영 데이터 파싱 함수 추가  - 진영 파싱은 이 함수 이용 예정

--- app/src/main/java/com/neppplus/colosseum_20211024/datas/SideData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/datas/SideData.kt
@@ -1,4 +1,26 @@
 package com.neppplus.colosseum_20211024.datas
 
+import org.json.JSONObject
+
 class SideData {
+
+    var id = 0  // Int가 들어올것이라고 명시
+    var title = "" // String이 들어올 자리라고 명시
+    var voteCount = 0
+
+    companion object {
+
+        fun getSideDataFromJSON( jsonObj: JSONObject ) : SideData {
+            val sideData = SideData()
+
+            sideData.id = jsonObj.getInt("id")
+            sideData.title = jsonObj.getString("title")
+            sideData.voteCount = jsonObj.getInt("vote_count")
+
+            return sideData
+
+        }
+
+    }
+
 }
