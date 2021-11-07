1e1ab7957ddbab2b37c13ac6b6f3d37aee40bcfe
nepp-plus <nepp.plus@gmail.com>
2021-11-07 14:35:13 +0900

    토론 상세 -> 댓글 목록 파싱  - 댓글목록 ArrayList에 추가

--- app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewTopicDetailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.neppplus.colosseum_20211024.databinding.ActivityViewTopicDetailBinding
+import com.neppplus.colosseum_20211024.datas.ReplyData
 import com.neppplus.colosseum_20211024.datas.TopicData
 import com.neppplus.colosseum_20211024.utils.ServerUtil
 import org.json.JSONObject
@@ -14,6 +15,8 @@ class ViewTopicDetailActivity : BaseActivity() {
     lateinit var binding:  ActivityViewTopicDetailBinding
 
     lateinit var mTopicData : TopicData
+
+    val mReplyList = ArrayList<ReplyData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +44,31 @@ class ViewTopicDetailActivity : BaseActivity() {
 
         ServerUtil.getRequestTopicDetail(mContext, mTopicData.id, "NEW", object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
+
+//                댓글 목록 JSONArrary -> 파싱 -> mReplyList의 자료로 추가
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicObj = dataObj.getJSONObject("topic")
+
+                val repliesArr = topicObj.getJSONArray("replies")
+
+                for ( i   in   0 until  repliesArr.length()  ) {
+
+                    val replyObj = repliesArr.getJSONObject(i)
+
+//                    JSONObject -> ReplyData 객체로 변환.
+                    val replyData = ReplyData()
+                    replyData.id =  replyObj.getInt("id")
+                    replyData.content = replyObj.getString("content")
+
+//                    댓글목록으로 추가
+                    mReplyList.add( replyData )
+
+                }
+
+//                리스트뷰의 목록에 변경 => 어댑터 새로고침
+
+
 
             }
 
