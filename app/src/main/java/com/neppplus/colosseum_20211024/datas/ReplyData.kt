8c75a50b7030732b84d3be373fa77faec0b2af79
nepp-plus <nepp.plus@gmail.com>
2021-11-14 10:18:14 +0900

    댓글 목록도 파싱 별도 함수 추가  - 좋아요 / 싫어요 / 댓글 갯수도 같이 파싱하게 추가

--- app/src/main/java/com/neppplus/colosseum_20211024/datas/ReplyData.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/datas/ReplyData.kt
@@ -1,9 +1,43 @@
 package com.neppplus.colosseum_20211024.datas
 
-class ReplyData(var id: Int, var content: String) {
+import org.json.JSONObject
+
+class ReplyData(
+    var id: Int,
+    var content: String) {
+
+    var likeCount = 0
+    var dislikeCount = 0
+    var myLike = false
+    var myDislike = false
+    var replyCount = 0
 
 //    var user : UserData  댓글을 적은 사람 정보 => 하위 데이터로.
     
     constructor() : this(0, "내용없음")
-    
+
+    companion object {
+
+//        jsonObj -> ReplyData로 변환해주는 기능
+
+        fun getReplyDataFromJson( jsonObj : JSONObject ) : ReplyData {
+
+            val  replyData = ReplyData()
+
+            replyData.id = jsonObj.getInt("id")
+            replyData.content = jsonObj.getString("content")
+
+            replyData.likeCount = jsonObj.getInt("like_count")
+            replyData.dislikeCount = jsonObj.getInt("dislike_count")
+            replyData.myLike = jsonObj.getBoolean("my_like")
+            replyData.myDislike = jsonObj.getBoolean("my_dislike")
+            replyData.replyCount = jsonObj.getInt("reply_count")
+
+
+            return replyData
+
+        }
+
+    }
+
 }
