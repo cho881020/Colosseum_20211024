06b218f8b9a0f133aec5b463a4a03cf202c3948f
nepp-plus <nepp.plus@gmail.com>
2021-11-14 12:37:35 +0900

    댓글 작성 화면 추가

---
+++ app/src/main/java/com/neppplus/colosseum_20211024/EditReplyActivity.kt
@@ -0,0 +1,28 @@
+package com.neppplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211024.databinding.ActivityEditReplyBinding
+
+class EditReplyActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditReplyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_reply)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
