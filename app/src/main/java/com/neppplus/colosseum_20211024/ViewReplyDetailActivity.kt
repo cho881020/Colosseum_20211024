a47993d607ec1f678986b15a3811a808f778d071
nepp-plus <nepp.plus@gmail.com>
2021-11-14 15:43:19 +0900

    받은 댓글데이터 이용해서 -> 어떤 댓글에 대한 화면인지 표시

--- app/src/main/java/com/neppplus/colosseum_20211024/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewReplyDetailActivity.kt
@@ -2,12 +2,19 @@ package com.neppplus.colosseum_20211024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211024.databinding.ActivityViewReplyDetailBinding
+import com.neppplus.colosseum_20211024.datas.ReplyData
 
 class ViewReplyDetailActivity : BaseActivity() {
 
+    lateinit var binding: ActivityViewReplyDetailBinding
+
+    lateinit var mReplyData : ReplyData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_reply_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_reply_detail)
         setupEvents()
         setValues()
     }
@@ -17,6 +24,11 @@ class ViewReplyDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        mReplyData = intent.getSerializableExtra("reply") as ReplyData
+
+        binding.writerNicknameTxt.text = mReplyData.user.nickname
+        binding.selectedSideTitleTxt.text = mReplyData.selectedSide.title
+        binding.contentTxt.text = mReplyData.content
 
     }
 
