ecab1970db3cd7c66fa0d9157f0586db0ccfac0f
nepp-plus <nepp.plus@gmail.com>
2021-11-14 16:01:32 +0900

    서버가 내려준 대댓글 목록을 액티비티의 ArrayList에 추가

--- app/src/main/java/com/neppplus/colosseum_20211024/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewReplyDetailActivity.kt
@@ -43,6 +43,19 @@ class ViewReplyDetailActivity : BaseActivity() {
         ServerUtil.getRequestReplyDetail(mContext, mReplyData.id, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(jsonObj: JSONObject) {
 
+                val dataObj = jsonObj.getJSONObject("data")
+                val replyObj = dataObj.getJSONObject("reply")
+
+                val repliesArr = replyObj.getJSONArray("replies")
+
+                for (i  in  0 until repliesArr.length()) {
+
+//                    위치에 맞는 JSONObject { } 추출 -> ReplyData로 변환 -> 대댓글 목록에 추가
+
+                    mReReplyList.add(  ReplyData.getReplyDataFromJson( repliesArr.getJSONObject(i) )  )
+
+                }
+
             }
 
         })
