0eabc871ec1f00ae2665480cbc33cfbb8996858d
nepp-plus <nepp.plus@gmail.com>
2021-11-14 15:20:37 +0900

    댓글 (의견) 상세 화면 UI 작업

---
+++ app/src/main/java/com/neppplus/colosseum_20211024/ViewReplyDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.neppplus.colosseum_20211024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewReplyDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_reply_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
